14c54b6c87b0667fd66d1ce3a6c3a8f12dffd0f9
nestor_leiva <nestorjoel2@gmail.com>
2024-06-12 00:15:28 -0600

    refactor 'optimizacion general'

--- TareaProgramada_1.py
+++ TareaProgramada_1.py
@@ -6,81 +6,64 @@ ventana = tk.Tk()
 
 """ -------------------------- Funciones -------------------------- """
 def labelTitulo(ventana, x , y):
-    label = tk.Label(ventana, text="Entrada de Numeros a Evaluar", font=("",15))
-    label.place(x=x, y=y)
-    #label.pack(padx=20, pady=20)
-
+    label = tk.Label(ventana, text="Entrada de Numeros a Evaluar", font=("",15)).place(x=x, y=y)
     # se realiza el posisionamiento del label por medio de las coordenadas
 #Fin label_Titulo
 
 def labelNumeros(ventana, x,y):
-    label = tk.Label(ventana,text="Ingrese los 3 numeros a Evaluar", font=("",12))
-    label.place(x=x, y=y)
+    label = tk.Label(ventana,text="Ingrese los 3 numeros a Evaluar", font=("",12)).place(x=x, y=y)
 #Fin label_Numeros
 
 def labelNumeroMayor(ventana, x,y):
-    label = tk.Label(ventana, text="Numero Mayor", font=("",12))
-    label.place(x=x,y=y)
+    label = tk.Label(ventana, text="Numero Mayor", font=("",12)).place(x=x,y=y)
 #Fin label_NumerosMayor
 
 def labelNumeroMenor(ventana, x,y):
-    label = tk.Label(ventana, text="Numero Menor", font=("",12))
-    label.place(x=x,y=y)
+    label = tk.Label(ventana, text="Numero Menor", font=("",12)).place(x=x,y=y)
 #Fin label_NumerosMenor
 
 def labelNumeroMayorMultiplicado(ventana, x,y):
-    label = tk.Label(ventana, text="Numero Mayor multiplicado & en Potencia", font=("",12))
-    label.place(x=x,y=y)
+    label = tk.Label(ventana, text="Numero Mayor multiplicado & en Potencia", font=("",12)).place(x=x,y=y)
 #Fin label_NumerosMayor&Potencia
 
 def labelNumeroMenorMultiplicado(ventana, x,y):
-    label = tk.Label(ventana, text="Numero Menor multiplicado & en Potencia", font=("",12))
-    label.place(x=x,y=y)
+    label = tk.Label(ventana, text="Numero Menor multiplicado & en Potencia", font=("",12)).place(x=x,y=y)
 #Fin label_NumerosMayor&Potencia
 
 def TexBoxNumero_1(ventana, x,y):
-    Texto = tk.Entry(ventana, width=10)
-    Texto.place(x=x, y=y)
+    Texto = tk.Entry(ventana, width=10, justify="center").place(x=x, y=y)
 #Fin textBoxNumero1
 
 def TexBoxNumero_2(ventana, x,y):
-    Texto = tk.Entry(ventana, width=10)
-    Texto.place(x=x, y=y)
+    Texto = tk.Entry(ventana, width=10, justify="center").place(x=x, y=y)
 #Fin textBoxNumero2
 
 def TexBoxNumero_3(ventana, x,y):
-    Texto = tk.Entry(ventana, width=10)
-    Texto.place(x=x, y=y)
+    Texto = tk.Entry(ventana, width=10, justify="center").place(x=x, y=y)
 #Fin textBoxNumero3
 
 def TexBoxNumero_Mayor(ventana, x,y):
-    Texto = tk.Entry(ventana, width=10)
-    Texto.place(x=x, y=y)
+    Texto = tk.Entry(ventana, width=10, justify="center", state="readonly").place(x=x, y=y)
 #Fin textBoxNumeroMayor
 
 def TexBoxNumero_Menor(ventana, x,y):
-    Texto = tk.Entry(ventana, width=10)
-    Texto.place(x=x, y=y)
+    Texto = tk.Entry(ventana, width=10, justify="center", state="readonly").place(x=x, y=y)
 #Fin textBoxNumeroMenor
 
 def TexBoxNumero_MayorMultiplicado(ventana, x,y):
-    Texto = tk.Entry(ventana, width=10)
-    Texto.place(x=x, y=y)
+    Texto = tk.Entry(ventana, width=10, justify="center", state="readonly").place(x=x, y=y)
 #Fin textBoxNumeroMayorMultiplicado
 
 def TexBoxNumero_MayorPotencia(ventana, x,y):
-    Texto = tk.Entry(ventana, width=10)
-    Texto.place(x=x, y=y)
+    Texto = tk.Entry(ventana, width=10, justify="center", state="readonly").place(x=x, y=y)
 #Fin textBoxNumeroMayorPotencia
 
 def TexBoxNumero_MenorMultiplicado(ventana, x,y):
-    Texto = tk.Entry(ventana, width=10)
-    Texto.place(x=x, y=y)
+    Texto = tk.Entry(ventana, width=10, justify="center", state="readonly").place(x=x, y=y)
 #Fin textBoxNumeroMenorMultiplicado
 
 def TexBoxNumero_MenorPotencia(ventana, x,y):
-    Texto = tk.Entry(ventana, width=10)
-    Texto.place(x=x, y=y)
+    Texto = tk.Entry(ventana, width=10, justify="center", state="readonly").place(x=x, y=y)
 #Fin textBoxNumeroMenorPotencia
 
 def BotonProcesar(ventana, x , y):
@@ -100,15 +83,12 @@ def BotonLimpiar(ventana, x , y):
     boton.place(x=x,y=y)
 #Boton Limpiar TextBox
 
-
 """ -------------------------- Funcion Main -------------------------- """
 
-ventana.title("Primera Tarea Programada") 
-# Titulo de la ventana
-ventana.geometry("700x500")
-# tamano de la ventana
+ventana.title("Primera Tarea Programada")   # Titulo de la ventana
+ventana.geometry("700x500") # tamano de la ventana
 
-""" *** Llamada de Funciones *** """
+# Llamado de las funciones con los parametros de 'Ventana' & los Ejes 'X', 'Y'
 labelTitulo(ventana, 200, 20)
 
 labelNumeros(ventana, 100, 120)
@@ -133,13 +113,8 @@ labelNumeroMenorMultiplicado(ventana, 30, 400)
 TexBoxNumero_MenorMultiplicado(ventana,350 , 400)
 TexBoxNumero_MenorPotencia(ventana, 450, 400)
 
-BotonTerminar(ventana, 375,450 )
-
+BotonTerminar(ventana, 375,450)
 
 ventana.mainloop()
 
-
-
-
-
-
+""" -------------------------- Fin Funcion Main -------------------------- """
