6324572bd13e3d1ad148b26ba08e6011b3aba8e1
nestor_leiva <nestorjoel2@gmail.com>
2024-07-23 15:48:01 -0600

    txt usuario / contrasena

--- TareaProgramada_4.py
+++ TareaProgramada_4.py
@@ -3,7 +3,7 @@ from tkinter import *
 
 ventana = tk.Tk()
 ventana.title('Tarea Programada 4')
-ventana.geometry('450x200')
+ventana.geometry('550x200')
 #---------------------------------------------------------------#
 def Limpiar(*TextoWidget):
     for texto in TextoWidget: texto.delete(0,"end")
@@ -43,20 +43,26 @@ def NuevaVentana(ventana):
     btnRegresar.grid(row= 6, column=1, columnspan=1, padx = 10, pady = 10,sticky="we")
 
 #---------------------------------------------------------------#
-lblTitulo = tk.Label(ventana, text="Ventana de Login",width=25, justify="center", bd=1, relief="solid", font=("",12))
+lblTitulo = tk.Label(ventana, text="Ventana de Login",width=25, justify="center", bd=2, relief="solid", font=("",12))
 lblTitulo.grid(row = 1, column = 1, columnspan=3, padx = 10, pady = 10,sticky="we")
 
-lblUsuario = tk.Label(ventana, text="Nombre de Usuario: ", width= 20,justify="center", bd=1, relief="solid", font=("",12))
+lblUsuario = tk.Label(ventana, text="Nombre de Usuario: ", width= 20,justify="center", bd=2, relief="solid", font=("",12))
 lblUsuario.grid(row=2, column=1, columnspan=1, padx = 10, pady = 10,sticky="we")
 
-lblContrasena = tk.Label(ventana, text="Contrasena: ", width= 20,justify="center", bd=1, relief="solid", font=("",12))
+txtUsuario = tk.Entry(ventana,  width= 20,justify="center", bd=2, relief="solid", font=("",12))
+txtUsuario.grid(row=2, column=2, columnspan=1, padx = 10, pady = 10,sticky="we")
+txtUsuario.focus()
+
+lblContrasena = tk.Label(ventana, text="Contrasena: ", width= 20,justify="center", bd=2, relief="solid", font=("",12))
 lblContrasena.grid(row=3, column=1, columnspan=1, padx = 10, pady = 10,sticky="we")
 
+txtContrasena = tk.Entry(ventana,  width= 20,justify="center", bd=2, relief="solid", font=("",12))
+txtContrasena.grid(row=3, column=2, columnspan=1, padx = 10, pady = 10,sticky="we")
 #---------------------------------------------------------------#
 btnLogin = tk.Button(ventana, text="Login",width=10, justify="center", bd=2, relief="solid", font=("",12), command=lambda:NuevaVentana(ventana))
 btnLogin.grid(row=4, column= 1, columnspan=1, padx=10, pady= 10, sticky='we' )
 
-btnLimpiar = tk.Button(ventana, text="Limpiar",width=10, justify="center", bd=2, relief="solid", font=("",12), command=lambda:Limpiar())
+btnLimpiar = tk.Button(ventana, text="Limpiar",width=10, justify="center", bd=2, relief="solid", font=("",12), command=lambda:Limpiar(txtUsuario,txtContrasena))
 btnLimpiar.grid(row=4, column= 2, columnspan=1, padx=10, pady= 10, sticky='we' )
 
 btnTerminar = tk.Button(ventana, text="Terminar",width=10, justify="center", bd=2, relief="solid", font=("",12), command=ventana.quit)
