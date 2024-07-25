8320d672e6c543ba807ccb4fdcb605cbcdd79f7f
nestor_leiva <nestorjoel2@gmail.com>
2024-07-25 15:07:46 -0600

    feat: def RealizarConsulta

--- TareaProgramada_4/TareaProgramada_4.py
+++ TareaProgramada_4/TareaProgramada_4.py
@@ -28,16 +28,16 @@ def NuevaVentana(ventana):
     lblRolUsuario = tk.Label(nuevaVentana, text="Rol de Usuario ",width=25, justify="center", bd=2, relief="solid", font=("",12))
     lblRolUsuario.grid(row= 5, column=1, columnspan=1, padx = 10, pady = 10,sticky="we")
     #---------------------------------------------------------------#
-    lblCodigoRes = tk.Label(nuevaVentana, text="",width=25, justify="left", bd=2, relief="solid", font=("",12))
+    lblCodigoRes = tk.Label(nuevaVentana, width=25, justify="left", bd=2, relief="solid", font=("",12))
     lblCodigoRes.grid(row= 2, column=2, columnspan=1, padx = 10, pady = 10,sticky="we")
     
-    lblDatosUsuarioRes = tk.Label(nuevaVentana, text="",width=25, justify="left", bd=2, relief="solid", font=("",12))
+    lblDatosUsuarioRes = tk.Label(nuevaVentana, width=25, justify="left", bd=2, relief="solid", font=("",12))
     lblDatosUsuarioRes.grid(row= 3, column=2, columnspan=1, padx = 10, pady = 10,sticky="we")
 
-    lblContrasenaUsuarioRes = tk.Label(nuevaVentana, text="",width=25, justify="left", bd=2, relief="solid", font=("",12))
+    lblContrasenaUsuarioRes = tk.Label(nuevaVentana, width=25, justify="left", bd=2, relief="solid", font=("",12))
     lblContrasenaUsuarioRes.grid(row= 4, column=2, columnspan=1, padx = 10, pady = 10,sticky="we")
 
-    lblRolUsuarioRes = tk.Label(nuevaVentana, text="",width=25, justify="left", bd=2, relief="solid", font=("",12))
+    lblRolUsuarioRes = tk.Label(nuevaVentana, width=25, justify="left", bd=2, relief="solid", font=("",12))
     lblRolUsuarioRes.grid(row= 5, column=2, columnspan=1, padx = 10, pady = 10,sticky="we")
     #---------------------------------------------------------------#
     btnRegresar = tk.Button(nuevaVentana, text="Regresar",width=25, justify="center", bd=2, relief="solid", font=("",12), command= nuevaVentana.destroy) # cierra la vetana
@@ -47,42 +47,64 @@ def SQLConexion():
     usuario = txtUsuario.get()
     contrasena = txtContrasena.get()
     server = "NESTORPC\\NESTOR"
-    database = "progra3"
+    database = "prograIII"
     username = "Nestor"
     password = "nestor123"
     username1 = "Arlin"
     password1 = "arlin123"
-    ConexionString = pyodbc.connect( f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};TrustServerCertificate=yes;')
-    ConexionString1 = pyodbc.connect( f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={database};UID={username1};PWD={password1};TrustServerCertificate=yes;')
-
-    if (usuario == username) and (contrasena == password):
+    ConexionString = pyodbc.connect(f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};TrustServerCertificate=yes;')
+    ConexionString1 = pyodbc.connect(f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={database};UID={username1};PWD={password1};TrustServerCertificate=yes;')
+   
+    if (usuario.strip() == username) and (contrasena.strip() == password):
         try:
             conexion = ConexionString
             print("Conexion Exitosa: ", username)
-            consulta = conexion.cursor()
-
-            consulta.execute('SELECT * FROM Usuarios WHERE Usuario = ?' , usuario) # ? evita la inyeccion de datos / es una consulta dinamica
-            for row in consulta:
-                print(row)
+            RealizarConsulta(conexion, usuario)
         except pyodbc.Error as e:
             print('Error al realizar la conexion', e)
         finally:
             conexion.close()
-            return True
-            ('se realizo el cierre de la conexion', conexion)
+            print('se realizo el cierre de la conexion',usuario,' ', conexion)
+            return True # Devuelve True si la conexión y autenticación fueron exitosas
+            
     elif (usuario == username1) and (contrasena == password1):    
         try:
             conexion1 = ConexionString1
             print("Conexion Exitosa: ", username1)
+            RealizarConsulta(conexion1, usuario)
         except pyodbc.Error as e:
             print('Error al realizar la conexion', e)
         finally:
             conexion1.close()
-            return True
-            print('se realizo el cierre de la conexion', conexion1)
+            print('se realizo el cierre de la conexion',usuario,' ', conexion1)
+            return True # Devuelve True si la conexión y autenticación fueron exitosas
     else:
         messagebox.showerror('Tarea Programada 4','Error al realizar la conexion a SQL')
-        return False
+        return False # Devuelve False si hubo un error en la conexión
+
+def RealizarConsulta(conexion, usuario):
+    try:
+        consulta = conexion.cursor()
+        query = """ SELECT Usuarios.Codigo, Usuarios.Usuario, Usuarios.Contra, Roles.Descripcion_rol
+                FROM Usuarios
+                INNER JOIN Roles ON Usuarios.Rol = Roles.Rol
+                INNER JOIN Estados ON Usuarios.Estado = Estados.Estado
+                WHERE Usuarios.Usuario = ? """
+        print(f'Ejecutando Consulta: {usuario}')
+        print(f'Query:  {query}')
+        print('Tupla: ',(usuario,))
+        consulta.execute(query, (usuario,))
+        #consulta.execute(query, 'Arlin')
+        for row in consulta:
+            print(row)
+    except pyodbc.Error as e:
+        #print('Error al realizar la consulta', e)
+        print('Error al realizar la consulta:')
+        print(f"SQLState: {e.args[0]}")
+        print(f"Driver: {e.args[1]}")
+        print(f"Message: {e.args[2]}")
+    finally:
+        consulta.close()
 
 def NuevoLogin():
     if SQLConexion():
