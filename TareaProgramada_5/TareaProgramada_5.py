c45d2de7232d1127192f43f251fa3de923373ffb
nestor_leiva <nestorjoel2@gmail.com>
2024-08-05 16:17:38 -0600

    feat: GUI def VentanaSecundaria

--- TareaProgramada_5/TareaProgramada_5.py
+++ TareaProgramada_5/TareaProgramada_5.py
@@ -3,11 +3,10 @@ from tkinter import messagebox
 import pyodbc
 
 ventana = tk.Tk()
-
 #-------------------------------------------#
 def Limpiar(*TextoWidget):
     for texto in TextoWidget: texto.delete(0,"end")
-def VentanaPrncipal():
+def VentanaLogin():
     ventana.title('Tarea Programada 5')
     ventana.geometry('500x200')
     lblTitulo = tk.Label(ventana, text="Ventana de Login",width=25, justify="center", bd=2, relief="solid", font=("",12))
@@ -34,6 +33,48 @@ def VentanaPrncipal():
 
     btnTerminar = tk.Button(ventana, text="Terminar",width=10, justify="center", bd=2, relief="solid", font=("",12), command=ventana.quit)
     btnTerminar.grid(row=4, column= 3, columnspan=1, padx=10, pady= 10, sticky='we' )
+
+def VentanaSecundaria(ventana):
+    nuevaVentana = tk.Toplevel(ventana)
+    nuevaVentana.geometry('650x150')
+    nuevaVentana.title('Tarea Programada 5')
+
+    lblNombreUsuario = tk.Label(nuevaVentana, text="Usuario:",width=15, justify="center", bd=2, relief="solid", font=("",12))
+    lblNombreUsuario.grid(row=0, column=0,  columnspan=1, padx = 10, pady = 10, sticky="we")
+    
+    lblNombreUsuarioRes = tk.Label(nuevaVentana, width=15, justify="center", bd=2, relief="solid", font=("",12))
+    lblNombreUsuarioRes.grid(row=0, column=1,  columnspan=1, padx = 5, pady = 5, sticky="we")
+    
+    lblCodigoUsuarioRes = tk.Label(nuevaVentana, width=15, justify="center", bd=2, relief="solid", font=("",12))
+    lblCodigoUsuarioRes.grid(row=0, column=2,  columnspan=1, padx = 5, pady = 5, sticky="we")
+
+    lblRolUsuarioRes = tk.Label(nuevaVentana, width=15, justify="center", bd=2, relief="solid", font=("",12))
+    lblRolUsuarioRes.grid(row=0, column=3,  columnspan=1, padx = 5, pady = 5, sticky="we")
+
+    lblConsulta = tk.Label(nuevaVentana, text="Que Accion desea Realizar",width=20, justify="center", bd=2, relief="solid", font=("",12))
+    lblConsulta.grid(row=3, column=0,  columnspan=4, padx = 5, pady = 5, sticky="we")
+    #-------------------------------------------#
+    btnCrear = tk.Button(nuevaVentana,text='Ingresar',width=15, justify="center", font=("",12))
+    btnCrear.grid(row=4, column=0,  columnspan=1, padx = 5, pady = 5, sticky="we")
+
+    btnLeer = tk.Button(nuevaVentana,text='Leer',width=15, justify="center", font=("",12))
+    btnLeer.grid(row=4, column=1,  columnspan=1, padx = 5, pady = 5, sticky="we")
+
+    btnModificar = tk.Button(nuevaVentana,text='Modificar',width=15, justify="center", font=("",12))
+    btnModificar.grid(row=4, column=2,  columnspan=1, padx = 5, pady = 5, sticky="we")
+
+    btnEliminar = tk.Button(nuevaVentana,text='Eliminar',width=15, justify="center", font=("",12))
+    btnEliminar.grid(row=4, column=3,  columnspan=1, padx = 5, pady = 5, sticky="we")
+
+
+def VentanaCRUD():
+    pass
+    # Crear /si crea usuario debe de crear login/
+    # Leer
+    # Actualizar
+    # Eliminar /si elimina usuario debe de eliminar login/
 #-------------------------------------------#
-VentanaPrncipal()
+VentanaLogin()
+VentanaSecundaria(ventana)
+
 ventana.mainloop()
