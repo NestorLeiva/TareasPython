ff6657eb23c5b1d8d12b3715534f1ddd85a13ad0
nestor_leiva <nestorjoel2@gmail.com>
2024-08-05 18:34:50 -0600

    feat: def VentanaCrear

--- TareaProgramada_5/TareaProgramada_5.py
+++ TareaProgramada_5/TareaProgramada_5.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
 import pyodbc
 
@@ -54,21 +55,80 @@ def VentanaSecundaria(ventana):
     lblConsulta = tk.Label(nuevaVentana, text="Que Accion desea Realizar",width=20, justify="center", bd=2, relief="solid", font=("",12))
     lblConsulta.grid(row=3, column=0,  columnspan=4, padx = 5, pady = 5, sticky="we")
     #-------------------------------------------#
-    btnCrear = tk.Button(nuevaVentana,text='Ingresar',width=15, justify="center", font=("",12))
+    btnCrear = tk.Button(nuevaVentana,text='Ingresar',width=15, justify="center", font=("",12), command=VentanaCrear)
     btnCrear.grid(row=4, column=0,  columnspan=1, padx = 5, pady = 5, sticky="we")
 
-    btnLeer = tk.Button(nuevaVentana,text='Leer',width=15, justify="center", font=("",12))
+    btnLeer = tk.Button(nuevaVentana,text='Leer',width=15, justify="center", font=("",12), command=VentanaLeer)
     btnLeer.grid(row=4, column=1,  columnspan=1, padx = 5, pady = 5, sticky="we")
 
-    btnModificar = tk.Button(nuevaVentana,text='Modificar',width=15, justify="center", font=("",12))
+    btnModificar = tk.Button(nuevaVentana,text='Modificar',width=15, justify="center", font=("",12), command=VentanaModificar)
     btnModificar.grid(row=4, column=2,  columnspan=1, padx = 5, pady = 5, sticky="we")
 
-    btnEliminar = tk.Button(nuevaVentana,text='Eliminar',width=15, justify="center", font=("",12))
+    btnEliminar = tk.Button(nuevaVentana,text='Eliminar',width=15, justify="center", font=("",12), command=VentanaEliminar)
     btnEliminar.grid(row=4, column=3,  columnspan=1, padx = 5, pady = 5, sticky="we")
 
+def VentanaCrear():
+    nuevaVentana = tk.Toplevel(ventana)
+    nuevaVentana.geometry('650x500')
+    nuevaVentana.title('Tarea Programada 5')
+    
+    # codigo aumentado automaticamente
+    lblUsuario = tk.Label(nuevaVentana, text="Nombre de Usuario:", width=20, justify="center", bd=2, relief="solid", font=("",12))
+    lblUsuario.grid(row=0, column=0,  columnspan=1, padx = 10, pady = 10, sticky="we")
 
-def VentanaCRUD():
-    pass
+    lblContrasenaUsuario = tk.Label(nuevaVentana, text="Contrasena de Usuario:", width=20, justify="center", bd=2, relief="solid", font=("",12))
+    lblContrasenaUsuario.grid(row=1, column=0,  columnspan=1, padx = 10, pady = 10, sticky="we")
+
+    lblNombreUsuario = tk.Label(nuevaVentana, text="Nombre:", width=20, justify="center", bd=2, relief="solid", font=("",12))
+    lblNombreUsuario.grid(row=2, column=0,  columnspan=1, padx = 10, pady = 10, sticky="we")
+
+    lblRolUsuario = tk.Label(nuevaVentana, text="Rol del Usuario:", width=20, justify="center", bd=2, relief="solid", font=("",12))
+    lblRolUsuario.grid(row=3, column=0,  columnspan=1, padx = 10, pady = 10, sticky="we")
+
+    lblEstadoUsuario = tk.Label(nuevaVentana, text="Estado del Usuario:",width=20, justify="center", bd=2, relief="solid", font=("",12))
+    lblEstadoUsuario.grid(row=4, column=0,  columnspan=1, padx = 10, pady = 10, sticky="we")
+    #-------------------------------------------#
+    txtUsuario = tk.Entry(nuevaVentana, width=20, justify="center", bd=2, relief="solid", font=("",12))
+    txtUsuario.grid(row=0, column=1,  columnspan=1, padx = 10, pady = 10, sticky="we")
+    txtUsuario.focus()
+ 
+    txtContrasenaUsuario = tk.Entry(nuevaVentana, width=20, justify="center", bd=2, relief="solid", font=("",12),show='*')
+    txtContrasenaUsuario.grid(row=1, column=1,  columnspan=1, padx = 10, pady = 10, sticky="we")
+
+    txtNombreUsuario = tk.Entry(nuevaVentana, width=20, justify="center", bd=2, relief="solid", font=("",12))
+    txtNombreUsuario.grid(row=2, column=1,  columnspan=1, padx = 10, pady = 10, sticky="we")
+
+    cboRolUsuario = ttk.Combobox(nuevaVentana, state="readonly", font=("", 12), values= ('Administrador????', 'Usuario????', 'Invitado????'))
+    cboRolUsuario.grid(row=3, column=1, columnspan=1, padx=10, pady=10, sticky="we")
+
+    cboEstadoUsuario = ttk.Combobox(nuevaVentana, state="readonly", font=("", 12), values= ('Activo', 'InActivo'))
+    cboEstadoUsuario.grid(row=4, column=1,  columnspan=1, padx = 10, pady = 10, sticky="we")
+
+
+
+def VentanaLeer():
+    nuevaVentana = tk.Toplevel(ventana)
+    nuevaVentana.geometry('650x500')
+    nuevaVentana.title('Tarea Programada 5')
+    lblNombreUsuario = tk.Label(nuevaVentana, text="Ventana Leer:",width=15, justify="center", bd=2, relief="solid", font=("",12))
+    lblNombreUsuario.grid(row=0, column=0,  columnspan=1, padx = 10, pady = 10, sticky="we")
+
+def VentanaModificar():
+    nuevaVentana = tk.Toplevel(ventana)
+    nuevaVentana.geometry('650x500')
+    nuevaVentana.title('Tarea Programada 5')
+    lblNombreUsuario = tk.Label(nuevaVentana, text="Ventana Modificar:",width=15, justify="center", bd=2, relief="solid", font=("",12))
+    lblNombreUsuario.grid(row=0, column=0,  columnspan=1, padx = 10, pady = 10, sticky="we")
+
+def VentanaEliminar():
+    nuevaVentana = tk.Toplevel(ventana)
+    nuevaVentana.geometry('650x500')
+    nuevaVentana.title('Tarea Programada 5')
+    lblNombreUsuario = tk.Label(nuevaVentana, text="Ventana Eliminar:",width=15, justify="center", bd=2, relief="solid", font=("",12))
+    lblNombreUsuario.grid(row=0, column=0,  columnspan=1, padx = 10, pady = 10, sticky="we")
+
+
+
     # Crear /si crea usuario debe de crear login/
     # Leer
     # Actualizar
@@ -76,5 +136,6 @@ def VentanaCRUD():
 #-------------------------------------------#
 VentanaLogin()
 VentanaSecundaria(ventana)
+
 
 ventana.mainloop()
