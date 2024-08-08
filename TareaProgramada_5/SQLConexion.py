5d224fe16bf102e2a30e36e92a181a8d39d2c856
nestor_leiva <nestorjoel2@gmail.com>
2024-08-08 01:07:46 -0600

    feat: def EscribirSQL

--- TareaProgramada_5/SQLConexion.py
+++ TareaProgramada_5/SQLConexion.py
@@ -38,11 +38,7 @@ class Conexion:
 
 # fin class Conexion
 
-
 class MetodosSQL:
-
-    def AceptarCambios():
-        print(messagebox.askyesno(message="¿Desea Continuar?", title="Tarea Programada 5"))
 
     def ValidarUsuario():
         #Usuario max 35 char
@@ -50,7 +46,7 @@ class MetodosSQL:
 
     def LeerUsuariosSQL(conn): # uso el parametro gobal para ingresar a la BD y realizar la consulta
         # cursor = senala una accion en BD
-        cursor = conn.cursor()
+        cursor = Conexion.conn.cursor()
         query = """ SELECT u.Codigo, u.Usuario, u.Nombre,r.Descripcion_rol, e.Descripcion_estado 
         FROM Usuarios u INNER JOIN Roles r ON u.Rol = r.Rol INNER JOIN Estados e ON u.Estado = e.Estado""" 
 
@@ -71,11 +67,31 @@ class MetodosSQL:
             
             nuevosDatos.append((codigo, usuario, nombre,rol,estado))
             print(nuevosDatos)
+        cursor.close() # se cierra la operacion
         return nuevosDatos
-
-    def EscribirSQL():
-        pass
         
+    def EscribirSQL(usuario, contrasena, nombre, rol, estado, CodigoMovimiento):
+        try:
+            cursor = Conexion.conn.cursor()
+            queryUsuario = """ INSERT INTO Usuarios (Usuario, Contra, Nombre, Rol, Estado) VALUES (?,?,?,?,?) """
+            cursor.execute(queryUsuario,usuario,contrasena,nombre,rol,estado)
+
+            cursor.execute("SELECT @@IDENTITY AS id") # selecciono el codigo declarado como identity = unico y autosumable
+            codigoUsuario = cursor.fetchone()[0] # obtengo el Identity
+
+            queryAuditoria = """ INSERT INTO Auditoria (codigo_usuario, codigo_movimiento, fecha_hora) VALUES(?,?,GETDATE()) """
+            #agrego los datos a la tabla Auditoria el valor GETDATE() da la fechahora 
+            cursor.execute(queryAuditoria, codigoUsuario, CodigoMovimiento) 
+
+            Conexion.conn.commit() # se aceptan los cambios 
+            messagebox.showinfo("Tarea Programada 5", " Usuario Ingresado Correctamente")
+            print("Tarea Programada 5", " Usuario Ingresado Correctamente")
+            print(f"Usuario agregado: Codigo: {codigoUsuario}, Usuario: {usuario}, Contrasena: {contrasena}, Nombre: {nombre}, Rol: {rol}, Estado: {estado}")
+            cursor.close() # cierro la operacion
+        except pyodbc.Error as e:
+            Conexion.conn.rollback() # si existe un error se rechaza la operacion
+            print(messagebox.showerror("Tarea Programada 5", f" Error al Ingresar el Usuario  {e}"))
+            
     def ActualizarSQL():
         pass
     def EliminarSQL():
@@ -85,7 +101,8 @@ class MetodosSQL:
 def prueba():
     if Conexion.LoginSQL(usuario="Nestor1", contrasena="nestor"):
         MetodosSQL.LeerUsuariosSQL(Conexion.conn)
+        MetodosSQL.EscribirSQL(Conexion.conn)
         Conexion.CerrarSQL()
 
 
-prueba()
+#prueba()
