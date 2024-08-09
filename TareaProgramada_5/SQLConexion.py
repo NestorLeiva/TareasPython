4ecaa5c36db23e62b6ce0445526cf2061f8ee8eb
nestor_leiva <nestorjoel2@gmail.com>
2024-08-09 01:11:10 -0600

    feat: def ConsultaSQL

--- TareaProgramada_5/SQLConexion.py
+++ TareaProgramada_5/SQLConexion.py
@@ -3,11 +3,10 @@ from tkinter import messagebox
 
 server = "NESTORPC\\NESTOR"
 database = "progra 3"
-global conn 
+global conn  # variable global para la conexion
 
 class Conexion:
-    # validar usuario activo / inactivo
-    conn = None # variable global para la conexion
+    conn = None # se le asigna un valor vacio
     def LoginSQL(usuario, contrasena):
         try:
             if usuario and contrasena:
@@ -21,7 +20,7 @@ class Conexion:
         except pyodbc.Error as e:
             messagebox.showerror("Tarea Programada 5", 'Usuario / Contrasena Incorrecto. \n Intente de nuevo')
             return False
-        # se validan las credenciales y si son validas se ingresa al programa y a la Base Datos. 
+    # Metodo para validar las credenciales y si son validas se ingresa al programa y a la Base Datos. 
 
     def CerrarSQL():
         try:
@@ -34,8 +33,7 @@ class Conexion:
                 messagebox.showinfo("Tarea Programada 5", "No hay conexión para cerrar")
         except pyodbc.Error as e:
             messagebox.showerror("Tarea Programada 5", f'Error al cerrar la conexión: {e}')
-        # metodo para cerrar la Sesion de la Base de Datos
-
+    # metodo para cerrar la Sesion de la Base de Datos
 # fin class Conexion
 
 class MetodosSQL:
@@ -49,18 +47,15 @@ class MetodosSQL:
         cursor.close()
     # Metodo para realizar el Insert en la Tabla Auditoria  BD
 
-        
-
     def LeerUsuariosSQL(conn): # uso el parametro gobal para ingresar a la BD y realizar la consulta
         # cursor = senala una accion en BD
         cursor = Conexion.conn.cursor()
         query = """ SELECT u.Codigo, u.Usuario, u.Contra, u.Nombre,r.Descripcion_rol, e.Descripcion_estado 
         FROM Usuarios u INNER JOIN Roles r ON u.Rol = r.Rol INNER JOIN Estados e ON u.Estado = e.Estado""" 
 
-        """ INNER JOIN combina las tablas
-        u/r/e son alias de las tablas
-        INNER JOIN Roles r ON u.Rol = r.Rol =  indica como se combinan las filas coincidiendo los campos
-        """
+        """ INNER JOIN = combina las tablas
+        u/r/e = son alias de las tablas
+        INNER JOIN Roles r ON u.Rol = r.Rol = indica como se combinan las filas coincidiendo los campos"""
         cursor.execute(query) # Se ejecuta el Query
 
         ResConsulta = cursor.fetchall() # obtengo todas las filas de BD 
@@ -73,11 +68,11 @@ class MetodosSQL:
             rol = consulta[4].strip()
             estado = consulta[5].strip()
             
-            nuevosDatos.append((codigo,usuario,contra, nombre,rol,estado))
+            nuevosDatos.append((codigo,usuario,contra, nombre,rol,estado)) # agrego los datos 
             print(nuevosDatos)
-        return nuevosDatos
-        
-        
+        return nuevosDatos 
+    # Metodo para realizar la lectura de las Tablas de BD
+     
     def EscribirSQL(codigo,usuario,contrasena, nombre,rol,estado,CodigoMovimiento):
         try:
             cursor = Conexion.conn.cursor()
@@ -95,8 +90,32 @@ class MetodosSQL:
             Conexion.conn.rollback() # si existe un error se rechaza la operacion
             print(messagebox.showerror("Tarea Programada 5", f" Error al Ingresar el Usuario  {e}"))
     # Metodo para realizar el Insert en la BD
+    
+    def ConsultaSQL(codigo):
+        try:
+            cursor = Conexion.conn.cursor()
+            queryConsulta = """SELECT u.Codigo, u.Usuario, u.Contra, u.Nombre, r.Descripcion_rol, e.Descripcion_estado 
+            FROM Usuarios u INNER JOIN Roles r ON u.Rol = r.Rol INNER JOIN Estados e ON u.Estado = e.Estado WHERE Codigo = ?"""
+            Qconsulta =  cursor.execute(queryConsulta, codigo)
             
+            datos = []
+            for consulta in Qconsulta.fetchall():
+                cod = int(consulta[0])
+                usuario = consulta[1].strip()
+                contra = consulta[2].strip()
+                nombre = consulta[3].strip()
+                rol = consulta[4].strip()
+                estado = consulta[5].strip()
+                datos.append((cod,usuario, contra,nombre,rol,estado)) # se crea una tupla
+                print('Consulta ',datos)
+                return datos
+        except pyodbc.Error as e:
+            messagebox.showerror("Tarea Programada 5", "Error al realizar la Consutla \n Verifique los datos")
+            print(f"Error al realizar la Consutla Verifique los datos {e}")
+    # Metodo para realizar la consulta de un usuario 
+
     def ModificarSQL():
+        
         pass
     def EliminarSQL():
         pass
@@ -104,7 +123,7 @@ class MetodosSQL:
 #-------------------------------------------------------------------------------------------------#
 def prueba():
     if Conexion.LoginSQL(usuario="Nestor1", contrasena="nestor"):
-        MetodosSQL.LeerUsuariosSQL(Conexion.conn, codigo=2 ,CodigoMovimiento=4)
-        #MetodosSQL.EscribirSQL(Conexion.conn)
+        #MetodosSQL.ModificarSQL(Conexion.conn, codigo=5)
+        MetodosSQL.ConsultaSQL(codigo=2)
         Conexion.CerrarSQL()
 #prueba()
