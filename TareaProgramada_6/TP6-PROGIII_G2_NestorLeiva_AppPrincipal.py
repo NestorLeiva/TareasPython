3a7fcb8ab29b5e2ca483ef278c8f5a0cba92272a
nestor_leiva <nestorjoel2@gmail.com>
2024-08-11 12:48:21 -0600

    feat: def __init__

--- TareaProgramada_6/TP6-PROGIII_G2_NestorLeiva_AppPrincipal.py
+++ TareaProgramada_6/TP6-PROGIII_G2_NestorLeiva_AppPrincipal.py
@@ -4,4 +4,15 @@ from tkinter import ttk
 from tkinter import messagebox
 
 class MiApp:
-    pass
+
+    def __init__(self, root):
+        self.ventana = root
+        self.ventana.title('Tarea Programada 6, "Cajero Automatico"')
+        self.ventana.geometry('500x500')
+    # Metodo Constructor de la clase
+    
+# Fin de class MiApp
+#-----------------------------------------------------------------------------------------#
+root = tk.Tk() # se crea la ventana
+app = MiApp(root) # se inicializa la aplicacion
+root.mainloop() # se inicializa el bucle principal
