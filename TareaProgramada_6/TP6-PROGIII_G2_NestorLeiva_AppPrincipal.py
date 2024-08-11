25b94e3d6ac071bdbb5eb3486ca74688bbc12354
nestor_leiva <nestorjoel2@gmail.com>
2024-08-11 13:25:44 -0600

    refactor: creacion def

--- TareaProgramada_6/TP6-PROGIII_G2_NestorLeiva_AppPrincipal.py
+++ TareaProgramada_6/TP6-PROGIII_G2_NestorLeiva_AppPrincipal.py
@@ -9,10 +9,46 @@ class MiApp:
         self.ventana = root
         self.ventana.title('Tarea Programada 6, "Cajero Automatico"')
         self.ventana.geometry('500x500')
+
     # Metodo Constructor de la clase
     
+    def VentanaOpciones(self):
+        pass
+    # Fin VentanaOpciones
+
+    def VentanaDeposito(self):
+        pass
+    # Fin VentanaDepositos
+
+    def VentanaRetiro(self):
+        pass
+    # Fin VentanaRetiro
+
+    def VentanaConsulta(self):
+        pass
+    # Fin VentanaConsulta
+
+    def VentanaCambioEstado(self):
+        pass
+    # Fin VentanCambioEstado
+    def EstadoLibre(self):
+        pass
+    #Fin EstadoLibre
+
+    def EstadoOcupado(self):
+        pass
+    #Fin Estado Ocupado
+
+    def EstadoMantenimiento(self):
+        pass
+    # Fin Estado Mantenimiento
+
+    def EstadoFueraServicio(self):
+        pass
+    # Fin Estado FueraServicio
+
 # Fin de class MiApp
 #-----------------------------------------------------------------------------------------#
-root = tk.Tk() # se crea la ventana
+root = tk.Tk() # se crea la ventana#
 app = MiApp(root) # se inicializa la aplicacion
 root.mainloop() # se inicializa el bucle principal
