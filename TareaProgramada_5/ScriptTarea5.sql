8fbdccc04d20c49674bdf8d8108f7540b16c4283
nestor_leiva <nestorjoel2@gmail.com>
2024-08-12 23:07:53 -0600

    refactor

--- TareaProgramada_5/ScriptTarea5.sql
+++ TareaProgramada_5/ScriptTarea5.sql
@@ -4,15 +4,6 @@
 */
 USE [master]
 GO
-/*Agregar el usuario al rol `securityadmin` si el login ya existe. 
-Esto debería ser ejecutado con una cuenta que tenga permisos suficientes (como `sa`)*/
--- Conceder permiso para crear logins
-GRANT ALTER ANY LOGIN TO Nestor1;
-GO
--- Agregar al usuario al rol securityadmin
-ALTER SERVER ROLE securityadmin ADD MEMBER Nestor1;
-GO
-
 
 CREATE DATABASE [Progra 3]
 GO
@@ -23,20 +14,32 @@ GO
 IF NOT EXISTS (SELECT * FROM sys.server_principals WHERE name = N'Nestor1')
 BEGIN
     CREATE LOGIN [Nestor1] WITH PASSWORD = N'nestor';
+	CREATE LOGIN [Arlin1] WITH PASSWORD = N'arlyn';
 END
 GO
 
 /*----------------------------------------------------*/
 -- Crear usuarios
-CREATE USER [Nestor_1] FOR LOGIN [Nestor1] WITH DEFAULT_SCHEMA=[dbo]
+CREATE USER [Nestor1] FOR LOGIN [Nestor1] WITH DEFAULT_SCHEMA=[dbo];
+CREATE USER [Arlyn1] FOR LOGIN [Arlyn1] WITH DEFAULT_SCHEMA=[dbo];
 GO
 
 /*----------------------------------------------------*/
--- Asignar roles a los usuarios
-ALTER ROLE [db_owner] ADD MEMBER [Nestor_1]
+-- Conceder permisos y agregar al rol securityadmin
+GRANT ALTER ANY LOGIN TO Nestor1;
+ALTER SERVER ROLE securityadmin ADD MEMBER Nestor1;
 GO
-ALTER ROLE [db_owner] ADD MEMBER [Arlin_1]
+
+/*Agregar el usuario al rol `securityadmin` si el login ya existe. 
+Esto debería ser ejecutado con una cuenta que tenga permisos suficientes (como `sa`)*/
+
+-- Conceder permiso para crear logins
+GRANT ALTER ANY LOGIN TO Nestor1;
 GO
+-- Agregar al usuario al rol securityadmin
+ALTER SERVER ROLE securityadmin ADD MEMBER Nestor1;
+GO
+
 -- Crear tablas
 /*----------------------------------------------------*/
 CREATE TABLE [dbo].[Usuarios](
@@ -76,20 +79,7 @@ GO
 CREATE TABLE [dbo].[Desc_Mov_Auditoria](
 	[codigo_movimiento]   numeric (5,0)  not null PRIMARY KEY,
 	[descripcion_movimiento] nvarchar(50) not null)
-/*----------------------------------------------------*/
--- Insertar datos
-INSERT INTO Usuarios (Codigo, Usuario, Contra, Nombre, Rol, Estado) VALUES(1, 'Arlyn', 'arlyn', 'Arlyn Madriz',1,1);
-INSERT INTO Usuarios (Codigo, Usuario, Contra, Nombre, Rol, Estado) VALUES(2, 'Nestor', 'nestor','Nestor Leiva',2,1);
-INSERT INTO Roles(Rol, Descripcion_rol) VALUES(1, 'Admin');
-INSERT INTO Roles(Rol, Descripcion_rol) VALUES(2, 'Conta');
-INSERT INTO Roles (Rol, Descripcion_rol) VALUES(3,'Supervisor');
-INSERT INTO Roles (Rol, Descripcion_rol) VALUES(4, 'Operario');
-INSERT INTO Estados(Estado, Descripcion_estado) VALUES(1, 'Activo');
-INSERT INTO Estados (Estado, Descripcion_estado) VALUES(2, 'InActivo');
-INSERT INTO Desc_Mov_Auditoria (codigo_movimiento, descripcion_movimiento) VALUES(1, 'Inserto Nuevo Registro');
-INSERT INTO Desc_Mov_Auditoria (codigo_movimiento, descripcion_movimiento) VALUES(2, 'Modifico Nuevo Registro');
-INSERT INTO Desc_Mov_Auditoria (codigo_movimiento, descripcion_movimiento) VALUES(3, 'Elimino Registro');
-INSERT INTO Desc_Mov_Auditoria (codigo_movimiento, descripcion_movimiento) VALUES(4, 'Consulta Registro');	
+
 /*----------------------------------------------------*/
 ALTER TABLE [dbo].[Usuarios]  WITH CHECK ADD  CONSTRAINT [FK_Usuarios_Estados] FOREIGN KEY([Estado])
 REFERENCES [dbo].[Estados] ([Estado])
@@ -123,8 +113,23 @@ REFERENCES [dbo].[Usuarios] ([Codigo])
 GO
 ALTER TABLE [dbo].[Auditoria] CHECK CONSTRAINT [FK_Auditoria_Usuarios]
 GO
-USE [master]
-GO
+
+/*----------------------------------------------------*/
+-- Insertar datos
+INSERT INTO Usuarios (Codigo, Usuario, Contra, Nombre, Rol, Estado) VALUES(1, 'Arlyn', 'arlyn', 'Arlyn Madriz',1,1);
+INSERT INTO Usuarios (Codigo, Usuario, Contra, Nombre, Rol, Estado) VALUES(2, 'Nestor', 'nestor','Nestor Leiva',2,1);
+INSERT INTO Roles(Rol, Descripcion_rol) VALUES(1, 'Admin');
+INSERT INTO Roles(Rol, Descripcion_rol) VALUES(2, 'Conta');
+INSERT INTO Roles (Rol, Descripcion_rol) VALUES(3,'Supervisor');
+INSERT INTO Roles (Rol, Descripcion_rol) VALUES(4, 'Operario');
+INSERT INTO Estados(Estado, Descripcion_estado) VALUES(1, 'Activo');
+INSERT INTO Estados (Estado, Descripcion_estado) VALUES(2, 'InActivo');
+INSERT INTO Desc_Mov_Auditoria (codigo_movimiento, descripcion_movimiento) VALUES(1, 'Inserto Nuevo Registro');
+INSERT INTO Desc_Mov_Auditoria (codigo_movimiento, descripcion_movimiento) VALUES(2, 'Modifico Nuevo Registro');
+INSERT INTO Desc_Mov_Auditoria (codigo_movimiento, descripcion_movimiento) VALUES(3, 'Elimino Registro');
+INSERT INTO Desc_Mov_Auditoria (codigo_movimiento, descripcion_movimiento) VALUES(4, 'Consulta Registro');	
+/*----------------------------------------------------*/
+
 -- Configurar base de datos para lectura y escritura
 ALTER DATABASE [Progra 3] SET  READ_WRITE 
 GO
